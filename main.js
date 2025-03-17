e0f988634bf25e5ce3d0e00374998e07ffe038ea
CRONighttrain <daswasdadwad1@gmail.com>
2025-03-16 19:31:25 -0700

    All the bugfixes

--- main.js
+++ main.js
@@ -1,18 +1,7 @@
 import { createBoard, playMove } from "./connect4.js";
 
-window.addEventListener("DOMContentLoaded", () => {
-  // Initialize the UI.
-  const board = document.querySelector(".board");
-  createBoard(board);
-  // Open the WebSocket connection and register event handlers.
-  const websocket = new WebSocket(getWebSocketServer());
-  initGame(websocket);
-  receiveMoves(board, websocket);
-  sendMoves(board, websocket);
-});
-
 function getWebSocketServer() {
-  if (window.location.host === "CRONighttrain.github.io") {
+  if (window.location.host === "cronighttrain2.github.io") {
     return "wss://connect4.koyeb.app/";
   } else if (window.location.host === "localhost:8000") {
     return "ws://localhost:8001/";
@@ -29,17 +18,54 @@ function initGame(websocket) {
     if (params.has("join")) {
       // Second player joins an existing game.
       event.join = params.get("join");
-    } if(params.has("watch")){
-      //person wants to watch
-      event.watch = params.get("watch")
-    }else {
+    } else if (params.has("watch")) {
+      // Spectator watches an existing game.
+      event.watch = params.get("watch");
+    } else {
       // First player starts a new game.
     }
     websocket.send(JSON.stringify(event));
   });
 }
 
+function showMessage(message) {
+  window.setTimeout(() => window.alert(message), 50);
+}
+
+function receiveMoves(board, websocket) {
+  websocket.addEventListener("message", ({ data }) => {
+    const event = JSON.parse(data);
+    switch (event.type) {
+      case "init":
+        // Create links for inviting the second player and spectators.
+        document.querySelector(".join").href = "?join=" + event.join;
+        document.querySelector(".watch").href = "?watch=" + event.watch;
+        break;
+      case "play":
+        // Update the UI with the move.
+        playMove(board, event.player, event.column, event.row);
+        break;
+      case "win":
+        showMessage(`Player ${event.player} wins!`);
+        // No further messages are expected; close the WebSocket connection.
+        websocket.close(1000);
+        break;
+      case "error":
+        showMessage(event.message);
+        break;
+      default:
+        throw new Error(`Unsupported event type: ${event.type}.`);
+    }
+  });
+}
+
 function sendMoves(board, websocket) {
+  // Don't send moves for a spectator watching a game.
+  const params = new URLSearchParams(window.location.search);
+  if (params.has("watch")) {
+    return;
+  }
+
   // When clicking a column, send a "play" event for a move in that column.
   board.addEventListener("click", ({ target }) => {
     const column = target.dataset.column;
@@ -55,33 +81,13 @@ function sendMoves(board, websocket) {
   });
 }
 
-function showMessage(message) {
-  window.setTimeout(() => window.alert(message), 50);
-}
-
-function receiveMoves(board, websocket) {
-  websocket.addEventListener("message", ({ data }) => {
-    const event = JSON.parse(data);
-    switch (event.type) {
-      case "init":
-        // Create link for inviting the second player.
-        document.querySelector(".join").href = "?join=" + event.join;
-        document.querySelector(".watch").href = "?watch=" + event.watch;
-        break;
-      case "play":
-        // Update the UI with the move.
-        playMove(board, event.player, event.column, event.row);
-        break;
-      case "win":
-        showMessage(`${event.player} wins!`);
-        // No further messages are expected; close the WebSocket connection.
-        websocket.close(1000);
-        break;
-      case "error":
-        showMessage(event.message);
-        break;
-      default:
-        throw new Error(`Unsupported event type: ${event.type}.`);
-    }
-  });
-}
+window.addEventListener("DOMContentLoaded", () => {
+  // Initialize the UI.
+  const board = document.querySelector(".board");
+  createBoard(board);
+  // Open the WebSocket connection and register event handlers.
+  const websocket = new WebSocket(getWebSocketServer());
+  initGame(websocket);
+  receiveMoves(board, websocket);
+  sendMoves(board, websocket);
+});
